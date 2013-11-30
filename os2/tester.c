0eac8caaa75503952f3a047e83a0364f043c0b24
henning <henningphan@hotmail.com>
2013-11-30 16:54:24 +0100

    Has a threadlist, code still broken

--- os2/tester.c
+++ os2/tester.c
@@ -13,46 +13,51 @@
 
 void fill100(int, int);
 void *dummy_Gustav(void*);
+int getIntSeed(void);
 
 int main(int argc, char *argv[] ) {
   struct timeval t1, t2;
-  double elapsedTime;
+  /* A magic struct that solves world hunger */
+  struct Package{
+      int work;
+      int seed;
+  };
   if( argc != 2){
-    printf("tester [Nr of threads]\n");
+    printf("Error: tester [Nr of threads]\n");
     exit(1);
   }
   int x =atoi(argv[1]);
   printf("%d\n", x);
-  
-  pthread_mutex_t *plist[x];
+
+  /* Check if input argument is correct*/
+  if ( x == 0){
+    perror("Invalid nr of threads requested");
+    exit(1);
+  }
+
+  pthread_t thread_id[x];
+  int size = sizeof(thread_id)/sizeof(thread_id[0]);
+  printf("Array size is %d\n", size);
   initialize_queue(); //create a queue
   gettimeofday(&t1, NULL); // Start stopwatch
   
-  int work = 1000/x;
+  int superseed = getIntSeed();
+  srand(superseed);
+  int work = 100000/x;
   printf("work: %d\n", work);
   int i = 0;
   for(i = 0; i < x; i++){
-    printf("creating a thread\n");
-    pthread_t thread;
-    pthread_create( &thread, NULL, dummy_Gustav, (void *) work);
-    plist[i] = &thread;
+    struct Package *package = malloc(sizeof(struct Package));
+    package->work = work;
+    package->seed = rand();
+    pthread_create( &thread_id[i], NULL, dummy_Gustav, (void *) &package);
   }
   printf("Waiting for threads\n");
   
   for( i = 0; i < x; i++){
-    pthread_join(plist[i], NULL);
+    pthread_join(thread_id[i], NULL);
   }
 
-/*
-  pthread_create( &thread1, NULL, dummy_Gustav, (void *) 10000);
-  pthread_join(thread1, NULL);
-
-  int *inten;
-  int i = 0;
-  for(i = 0; i < 200; i++) {
-	dequeue(inten);
-  }
-  */
 
   // Stop stopwatch
   gettimeofday(&t2, NULL); // Stop stopwatch
@@ -68,6 +73,13 @@ void fill100(int x, int iterations) {
   }
 }
 void *dummy_Gustav(void *arg) {
+    struct Package *pack = arg;
+    printf("Thread seed: %d\n", pack->seed);
 //    fill100(1, (int *) arg);
 
+}
+int getIntSeed(void){
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return t.tv_usec;
 }
